8fd55cbbd0ebccbc278042c3691b68d490e5e4db
EdgarPons <edgarponsforn@gmail.com>
2023-01-02 21:39:02 +0100

    Create Madd_2022.ino

---
+++ Madd_2022/Madd_2022.ino
@@ -0,0 +1,48 @@
+////// Time controller //////
+unsigned long previousMillis = 0;
+const long interval = 10000;
+
+////// SCD41 Libraries //////
+#include <Wire.h>
+#include "SparkFun_SCD4x_Arduino_Library.h"
+SCD4x mySensor;
+
+void setup() {
+  Serial.begin(115200);
+
+  ////// SCD41 Setup //////
+  Wire.begin();
+  if (mySensor.begin() == false) {
+    Serial.println(F("SCD41 Sensor not detected. Please check wiring."));
+    while (1)
+      ;
+  }
+}
+
+void recordValues() {
+  ////// SCD41 Setup //////
+  if (mySensor.readMeasurement())  // readMeasurement will return true when fresh data is available
+  {
+    Serial.print(F("CO2(ppm):"));
+    Serial.print(mySensor.getCO2());
+
+    Serial.print(F("\tTemperature(C):"));
+    Serial.print(mySensor.getTemperature(), 1);
+
+    Serial.print(F("\tHumidity(%RH):"));
+    Serial.print(mySensor.getHumidity(), 1);
+
+    Serial.println();
+  }
+}
+
+
+void loop() {
+
+  ////// Time controller //////
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    recordValues();
+  }
+}
