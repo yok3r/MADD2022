40d8d9abe28f4d7399986ca70f56bd93a6d09f3f
EdgarPons <edgarponsforn@gmail.com>
2023-01-04 15:42:33 +0100

    Updates

--- Madd_2022/Madd_2022.ino
+++ Madd_2022/Madd_2022.ino
@@ -1,6 +1,6 @@
 ////// Time controller //////
 unsigned long previousMillis = 0;
-const long interval = 10000;
+const long interval = 5000;
 
 ////// SCD41 Libraries ////// - Sparkfun SCD4X
 #include <Wire.h>
@@ -9,22 +9,34 @@ SCD4x mySensor;
 
 ////// PM25 Libraries ////// - Adafruit PM25 AQI
 #include "Adafruit_PM25AQI.h"
+#include <SoftwareSerial.h>
+SoftwareSerial pmSerial(2, 3);
+Adafruit_PM25AQI aqi = Adafruit_PM25AQI();
 
 
 void setup() {
   Serial.begin(115200);
+  // Wait one second for sensor to boot up!
+  delay(1000);
+
+  ////// PM25 Setup //////
+  pmSerial.begin(9600);
+  if (!aqi.begin_UART(&pmSerial)) {  // connect to the sensor over software serial
+    Serial.println("Could not find PM 2.5 sensor!");
+    while (1) delay(10);
+  }
 
   ////// SCD41 Setup //////
   Wire.begin();
   if (mySensor.begin() == false) {
-    Serial.println(F("SCD41 Sensor not detected. Please check wiring."));
+    Serial.println(F("SCD41 Sensor not detected."));
     while (1)
       ;
   }
 }
 
 void recordValues() {
-  ////// SCD41 Setup //////
+  ////// SCD41 Reading //////
   if (mySensor.readMeasurement())  // readMeasurement will return true when fresh data is available
   {
     Serial.print(F("CO2(ppm):"));
@@ -38,6 +50,19 @@ void recordValues() {
 
     Serial.println();
   }
+
+  ////// PM25 Reading //////
+  PM25_AQI_Data data;
+
+  Serial.println(F("---------------------------------------"));
+  Serial.print(F("PM 1.0: "));
+  Serial.print(data.pm10_standard);
+  Serial.print(F("\t\tPM 2.5: "));
+  Serial.print(data.pm25_standard);
+  Serial.print(F("\t\tPM 10: "));
+  Serial.println(data.pm100_standard);
+  Serial.println(F("Concentration Units (environmental)"));
+  Serial.println(F("---------------------------------------"));
 }
 
 
@@ -48,5 +73,6 @@ void loop() {
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
     recordValues();
+
   }
 }
